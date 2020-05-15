13f3f0689f5e0fcfc4105bb46768d13fdadfe8e9
Steven De Ryck <steven@inform.be>
2020-05-15 15:21:41 +0200

    Aanpassing User klasse

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,7 +58,7 @@ class User implements \Symfony\Component\Security\Core\User\UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+        // guarantee every user at least has ROLE_USER **
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
